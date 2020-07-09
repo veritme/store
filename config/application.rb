3ac6711781dd3ff5cceefa5391efb68dfecbef53
bryan zamora <bryan_ala@live.com>
2020-07-09 14:04:37 -0500

    Mi primer commit

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Spree
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
 
     config.to_prepare do
       # Load application's model / class decorators
