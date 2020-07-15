cd299b8ca796b15571c6b7d43637931d6ab8057a
bryan zamora <bryan_ala@live.com>
2020-07-15 15:17:27 -0500

    add carousel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,10 +35,20 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain:"gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "smarthouse.soporte@gmail.com",
+    password: "16zamora"
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-  
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
